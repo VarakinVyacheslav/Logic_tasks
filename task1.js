55724fc37b950a22cd9041651359ce6ea338b063
Varakin Vyacheslav <varakin.vyacheslavv@gmail.com>
2021-07-02 12:06:33 +0300

    added array checking

--- task1.js
+++ task1.js
@@ -1,5 +1,9 @@
 //Function that takes array of numbers and returns minimum and maximum values
 let min_max = (array) => {
+	if (Array.isArray(array) == false)
+		{
+			return "Not array";
+		}
 	let min = Infinity;
 	let max = -Infinity;
 	for (let i = 0; i < array.length; i++)
@@ -14,3 +18,4 @@ let min_max = (array) => {
 }
 let array = [1, 10, -30, 3, 12, -55, 103];	//[-55, 103]
 console.log(min_max(array));
+console.log(min_max(2));
--- task2.js
+++ task2.js
@@ -1,13 +1,21 @@
 
 //function that takes array of numbers and returns average value
 let avr = (array) => {
+	if ( array instanceof Array == false)
+		{
+			return "Not an Array";
+		}
+	if (array.length == 0)
+		return "empty";
 	let sum = 0;
 	for (let i = 0; i < array.length; i++) //getting the sum of array values
 		{sum = sum + array[i];}
 	return sum / array.length;
 }
 
-let array1 = [1, 10, -30, 3, 12, -55, 103];	//6.2857
-let array2 = [32, 11, -30, 8, 26, -23, 53];	//11
-console.log(avr(array1));
-console.log(avr(array2));
+let array1 = [1, 10, -30, 3, 12, -55, 103];	
+let array2 = [32, 11, -30, 8, 26, -23, 53];	
+console.log(avr(array1));	//6.2857
+console.log(avr(array2));	//11
+console.log(avr(2));		//"Not an Array"
+console.log(avr([]));		//"empty"
