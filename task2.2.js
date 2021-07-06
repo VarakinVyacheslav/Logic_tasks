88425ba53a332cca68ab96d24de449c4ed1329f1
Varakin Vyacheslav <varakin.vyacheslavv@gmail.com>
2021-07-06 13:43:13 +0300

    recursive sequence addition function

--- task2.2.js
+++ task2.2.js
@@ -1,12 +1,14 @@
 //sum of number sequences
 let Add = (n) => {
+	if (n < 0) {
+		return "Only positive numbers";
+	}
 	if (n == 1) {
 		return 1;
 	}
 
 	return n + Add(n - 1);
 }
-
 console.log( Add(-20) );
 console.log( Add(3) ); 
 console.log( Add(5) );	
