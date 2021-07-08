5482cc04cbdb331e65286322b7bb3c363c7268ed
Varakin Vyacheslav <varakin.vyacheslavv@gmail.com>
2021-07-08 16:02:00 +0300

    Fixed indents

--- task1.js
+++ task1.js
@@ -1,25 +1,25 @@
 //Function that takes array of numbers and returns minimum and maximum values
 let min_max = (array) => {
-	if (Array.isArray(array) == false) {
-		return "Not array";
-	}
+    if (Array.isArray(array) == false) {
+        return "Not array";
+    }
 
-	let min = Infinity;
-	let max = -Infinity;
+    let min = Infinity;
+    let max = -Infinity;
 
-	for (let i = 0; i < array.length; i++) {
-		if (array[i] < min){
-			min = array[i];
-		}
-	}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
 
-	for (let i = 0; i < array.length; i++) {
-		if (array[i] > max) {
-			max = array[i];
-		}
-	}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
 
-	return [min, max];
+    return [min, max];
 }
 let array = [1, 10, -30, 3, 12, -55, 103];	//[-55, 103]
 console.log( min_max(array) );
